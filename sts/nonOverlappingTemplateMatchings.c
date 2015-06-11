ab7c507877d0586a143290f27e7d7fcc477a981c
drazil <abcdefghi236@gmail.com>
2015-06-11 17:49:42 +0000

    minor fix of rng test

--- sts/nonOverlappingTemplateMatchings.c
+++ sts/nonOverlappingTemplateMatchings.c
@@ -22,94 +22,98 @@ double NonOverlappingTemplateMatchings(int m, int n, unsigned char *epsilon)
 	unsigned int	bit, W_obs, nu[6], *Wj = NULL; 
 	FILE			*fp = NULL;
 	double			sum, chi2, p_value, g_p_value = 1.0, lambda, pi[6], varWj;
-	int				i, j, jj, k, match, SKIP, M, N, K = 5;
+	int				i, j, jj, k, match, /*SKIP, */M, N, K = 5;
 	char			directory[100];
 	unsigned char		*sequence = NULL;
 
 	N = 8;
 	M = n/N;
 
-	if ( (Wj = (unsigned int*)calloc(N, sizeof(unsigned int))) == NULL ) {
-		return -1.0;
-	}
-	lambda = (M-m+1)/pow(2, m);
-	varWj = M*(1.0/pow(2.0, m) - (2.0*m-1.0)/pow(2.0, 2.0*m));
-	sprintf(directory, "templates/template%d", m);
+  if ( (Wj = (unsigned int*)calloc(N, sizeof(unsigned int))) == NULL ) {
+    return -1.0;
+  }
+  lambda = (M-m+1)/pow(2.0, m);
+  varWj = (1.0*M/pow(2.0, m) - (2.0*m-1.0)*M/pow(2.0, 2.0*m));
+  sprintf(directory, "templates/template%d", m);
 
-	if ( ((isNegative(lambda)) || (isZero(lambda))) ||
-		 ((fp = fopen(directory, "r")) == NULL) ||
-		 ((sequence = (unsigned char *) calloc(m, sizeof(unsigned char))) == NULL) ) {
-		if ( sequence != NULL )
-			free(sequence);
+  if ( ((isNegative(lambda)) || (isZero(lambda))) ||
+      ((fp = fopen(directory, "r")) == NULL) ||
+      ((sequence = (unsigned char *) calloc(m, sizeof(unsigned char))) == NULL) ) {
+    if ( sequence != NULL )
+      free(sequence);
     g_p_value = -1.0;
-	}
-	else {
+  }
+  else {
 
-		if ( numOfTemplates[m] < MAXNUMOFTEMPLATES )
-			SKIP = 1;
-		else
-			SKIP = (int)(numOfTemplates[m]/MAXNUMOFTEMPLATES);
-		numOfTemplates[m] = (int)numOfTemplates[m]/SKIP;
-		
-		sum = 0.0;
-		for ( i=0; i<2; i++ ) {                      /* Compute Probabilities */
-			pi[i] = exp(-lambda+i*log(lambda)-cephes_lgam(i+1));
-			sum += pi[i];
-		}
-		pi[0] = sum;
-		for ( i=2; i<=K; i++ ) {                      /* Compute Probabilities */
-			pi[i-1] = exp(-lambda+i*log(lambda)-cephes_lgam(i+1));
-			sum += pi[i-1];
-		}
-		pi[K] = 1 - sum;
+    /*
+       if ( numOfTemplates[m] < MAXNUMOFTEMPLATES )
+       SKIP = 1;
+       else
+       SKIP = (int)(numOfTemplates[m]/MAXNUMOFTEMPLATES);
+       numOfTemplates[m] = (int)numOfTemplates[m]/SKIP;
+       */
 
-		for( jj=0; jj<MIN(MAXNUMOFTEMPLATES, numOfTemplates[m]); jj++ ) {
-			sum = 0;
+    sum = 0.0;
+    for ( i=0; i<2; i++ ) {                      /* Compute Probabilities */
+      pi[i] = exp(-lambda+i*log(lambda)-cephes_lgam(i+1));
+      sum += pi[i];
+    }
+    pi[0] = sum;
+    for ( i=2; i<=K; i++ ) {                      /* Compute Probabilities */
+      pi[i-1] = exp(-lambda+i*log(lambda)-cephes_lgam(i+1));
+      sum += pi[i-1];
+    }
+    pi[K] = 1 - sum;
 
-			for ( k=0; k<m; k++ ) {
-				fscanf(fp, "%d", &bit);
-				sequence[k] = bit;
-			}
-			for ( k=0; k<=K; k++ )
-				nu[k] = 0;
-			for ( i=0; i<N; i++ ) {
-				W_obs = 0;
-				for ( j=0; j<M-m+1; j++ ) {
-					match = 1;
-					for ( k=0; k<m; k++ ) {
-						if ( (int)sequence[k] != (int)epsilon[i*M+j+k] ) {
-							match = 0;
-							break;
-						}
-					}
-					if ( match == 1 ) {
-						W_obs++;
-                        j += m-1;
-                    }
-				}
-				Wj[i] = W_obs;
-			}
-			sum = 0;
-			chi2 = 0.0;                                   /* Compute Chi Square */
-			for ( i=0; i<N; i++ ) {
-				chi2 += pow(((double)Wj[i] - lambda)/pow(varWj, 0.5), 2);
-			}
-			p_value = cephes_igamc(N/2.0, chi2/2.0);
+    for( jj=0; jj</*MIN(MAXNUMOFTEMPLATES, */numOfTemplates[m]/*)*/; jj++ ) {
+      sum = 0;
+
+      for ( k=0; k<m; k++ ) {
+        fscanf(fp, "%d", &bit);
+        sequence[k] = bit;
+      }
+      for ( k=0; k<=K; k++ )
+        nu[k] = 0;
+      for ( i=0; i<N; i++ ) {
+        W_obs = 0;
+        for ( j=0; j<M-m+1; j++ ) {
+          match = 1;
+          for ( k=0; k<m; k++ ) {
+            if ( (int)sequence[k] != (int)epsilon[i*M+j+k] ) {
+              match = 0;
+              break;
+            }
+          }
+          if ( match == 1 ) {
+            W_obs++;
+            j += m-1;
+          }
+        }
+        Wj[i] = W_obs;
+      }
+      sum = 0;
+      chi2 = 0.0;                                   /* Compute Chi Square */
+      for ( i=0; i<N; i++ ) {
+        chi2 += pow((double)Wj[i] - lambda, 2.0)/varWj ;
+      }
+      p_value = cephes_igamc(N/2.0, chi2/2.0);
 
       if(p_value < g_p_value)
         g_p_value = p_value ;
-		
-			if ( SKIP > 1 )
-				fseek(fp, (long)(SKIP-1)*2*m, SEEK_CUR);
-		}
-	}
-	
-	if ( sequence != NULL )
-		free(sequence);
 
-	free(Wj);
-    if ( fp != NULL )
-        fclose(fp);
+      /*
+         if ( SKIP > 1 )
+         fseek(fp, (long)(SKIP-1)*2*m, SEEK_CUR);
+         */
+    }
+  }
+
+  if ( sequence != NULL )
+    free(sequence);
+
+  free(Wj);
+  if ( fp != NULL )
+    fclose(fp);
 
   return g_p_value ;
 }
--- sts/test.cpp
+++ sts/test.cpp
@@ -30,9 +30,15 @@ unsigned char eps[MY_N] ;
 
 int main(void)
 {
-  std::random_device rd("/dev/random") ;
-  for(int i=0;i<MY_N;i++) eps[i] = rd()%2 ;
-  //for(int i=0;i<MY_N;i++) eps[i] = rand()%2 ;
+  /*
+  for(int i=0;i<MY_N;i++)
+  {
+    scanf("%c", &eps[i]) ;
+    eps[i] -= '0' ;
+    if(eps[i]!=0 && eps[i]!=1) i-- ;
+  }
+  */
+  for(int i=0;i<MY_N;i++) eps[i] = rand()%2 ;
   
   std::cout << Frequency(MY_N, eps) << std::endl ;
   std::cout << BlockFrequency(MY_N/100, MY_N, eps) << std::endl ;
